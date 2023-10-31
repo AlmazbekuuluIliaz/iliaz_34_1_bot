39ab17f7b4e89b47171ed1bf30ab6ca127703f22
YOUR USERNAME ON GITHUB <YOUR EMAIL ON GITHUB>
2023-10-31 14:44:24 +0300

    first lesson

--- handlers/start.py
+++ handlers/start.py
@@ -1,8 +1,7 @@
 import sqlite3
-
 from aiogram import types, Dispatcher
 from config import bot
-# from database.sql_commands import Database
+from database.sql_commands import Database
 from keyboards.inline_buttons import start_keyboard
 
 
