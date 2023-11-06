823c56d17bc466722dd8c9009bd60079aeb25492
YOUR USERNAME ON GITHUB <YOUR EMAIL ON GITHUB>
2023-11-06 04:42:52 +0300

    sixth lesson

--- handlers/callback.py
+++ handlers/callback.py
@@ -1,6 +1,7 @@
 from aiogram import types, Dispatcher
 from config import bot
 from keyboards.inline_buttons import questionnaire_one_keyboard
+from scraping.news_scraper import NewsScraper
 
 
 async def start_questionnaire(call: types.CallbackQuery):
@@ -30,8 +31,14 @@ async def no_answer(call: types.CallbackQuery):
         chat_id=call.message.chat.id,
         text="Glad you r not hungry",
     )
-
-
+async def latest_news_call(call: types.CallbackQuery):
+    scraper = NewsScraper()
+    links = scraper.parse_data()
+    for link in links:
+        await bot.send_message(
+            chat_id=call.message.chat.id,
+            text=scraper.PLUS_URL + link,
+        )
 def register_callback_handlers(dp: Dispatcher):
     dp.register_callback_query_handler(start_questionnaire,
                                        lambda call: call.data == "start_questionnaire")
@@ -39,3 +46,5 @@ def register_callback_handlers(dp: Dispatcher):
                                        lambda call: call.data == "hungry_yes")
     dp.register_callback_query_handler(no_answer,
                                        lambda call: call.data == "hungry_no")
+    dp.register_callback_query_handler(latest_news_call,
+                                       lambda call: call.data == "latest_news")
--- keyboards/inline_buttons.py
+++ keyboards/inline_buttons.py
@@ -24,11 +24,16 @@ async def start_keyboard():
         "Referral Menu 💵",
         callback_data="reference_menu"
     )
+    latest_news_button = InlineKeyboardButton(
+        "3 Latest News",
+        callback_data="latest_news"
+    )
     markup.add(questionnaire_button)
     markup.add(registration_button)
     markup.add(my_profile_button)
     markup.add(random_profile_button)
     markup.add(reference_menu_button)
+    markup.add(latest_news_button)
     return markup
 
 
